8822e23e01930512adafb6c1ec5dc86537568ec5
kevin cuadros <kevindavidcuadros@gmail.com>
2025-11-10 17:06:49 -0500

    Fix getNotFollowingMe function to use correct property for comparison

--- src/utils/compare-arrays.ts
+++ src/utils/compare-arrays.ts
@@ -3,9 +3,9 @@ import type { Followers } from "../interfaces/followers.interface";
 export function getNotFollowingMe( followerList: Followers[], followingList: Followers[] ): Followers[]{
   const notFollowing: Followers[] = []
   const followerValues = new Set(followerList.map(follower => follower.string_list_data[0].value));
-
+  
   for (const follow of followingList) {
-    if (!followerValues.has(follow.string_list_data[0].value)) {
+    if (!followerValues.has(follow.title)) {
       notFollowing.push(follow);
     }
   }
